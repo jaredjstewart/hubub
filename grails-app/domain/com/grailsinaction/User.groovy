43e5b16c0c1e3b9fcfb11768a91c6ad9aba7aaa5
jaredjstewart <stewart.jared@gmail.com>
2014-07-05 13:37:39 -0500

    Initial commit

---
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -0,0 +1,14 @@
+package com.grailsinaction
+class User {
+    String loginId
+    String password
+    Date dateCreated
+    static hasOne = [ profile : Profile ]
+    static constraints = {
+        loginId size: 3..20, unique: true, blank: false
+        password size: 6..8, blank: false, validator: { passwd, user ->
+            passwd != user.loginId
+        }
+        profile nullable: true
+    }
+}
