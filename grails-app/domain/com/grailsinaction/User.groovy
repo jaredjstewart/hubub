b0c6f8840e8d6f2af1d7eb4c0c39cd4bdfdc400c
jaredjstewart <stewart.jared@gmail.com>
2014-07-05 13:50:28 -0500

    Post domain class added, tests passing.

--- grails-app/domain/com/grailsinaction/User.groovy
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -1,9 +1,11 @@
 package com.grailsinaction
+
 class User {
     String loginId
     String password
     Date dateCreated
-    static hasOne = [ profile : Profile ]
+    static hasOne = [profile: Profile]
+    static hasMany = [posts: Post]
     static constraints = {
         loginId size: 3..20, unique: true, blank: false
         password size: 6..8, blank: false, validator: { passwd, user ->
